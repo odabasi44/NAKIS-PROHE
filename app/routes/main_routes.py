31b5c9a2912bc6d53500220f2bebbe08dba6fa46
AHMET ODABASI <77965207+odabasi44@users.noreply.github.com>
2025-12-10 01:58:37 +0300

    Update main_routes.py

--- app/routes/main_routes.py
+++ app/routes/main_routes.py
@@ -1,1 +1,16 @@
+import os
+from flask import Blueprint, render_template, redirect
 
+bp = Blueprint('main', __name__)
+
+@bp.route("/")
+def home():
+    return render_template("index.html")
+
+@bp.route("/<page>")
+def render_page(page):
+    # Basit bir sayfa yönlendiricisi
+    # templates klasöründe <page>.html varsa onu açar, yoksa ana sayfaya atar.
+    if os.path.exists(f"templates/{page}.html"):
+        return render_template(f"{page}.html")
+    return redirect("/")
